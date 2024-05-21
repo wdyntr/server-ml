92a87f48baa117554e8dcfb3813116c2673d1213
wdyntr <widyantoro0603@gmail.com>
2024-05-21 15:47:19 +0700

    update

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const InputError = require('../exceptions/InputError');
 
 (async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: 8080,
         host: 'localhost',
         routes: {
             cors: {
