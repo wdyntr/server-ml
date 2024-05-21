c55736e68606aba58702be11910a89d9e7b3edb5
wdyntr <widyantoro0603@gmail.com>
2024-05-21 17:32:37 +0700

    waduh

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,9 +1,8 @@
 const { Firestore } = require('@google-cloud/firestore');
 
-// Pastikan Anda memiliki credential atau proyek ID yang benar
 const firestore = new Firestore({
   projectId: 'submissionmlgc-m-widyantoro-w',
-  keyFilename: './serviceAccountKey.json', // Atau sesuaikan dengan lokasi file kredensial Anda
+  keyFilename: './serviceAccountKey.json', 
 });
 
 async function storeData(id, data) {
